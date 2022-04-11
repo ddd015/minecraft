43d32604f9bc6854e55ae7c2daf4987c33d4132e
ddd015 <101343055+ddd015@users.noreply.github.com>
2022-04-11 13:40:34 +0300

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,9 +1,3 @@
-node 'slave1' {
-  include static_conf
-}
-node 'slave2' {
-  include dynamic_conf
-}
 node 'master' {
 include nginx
 nginx::resource::server { 'static':
@@ -13,61 +7,5 @@ nginx::resource::server { 'static':
 nginx::resource::server { 'dynamic':
   listen_port => 81,
   proxy => 'http://192.168.56.7:80',
-  }
-}
-
-class dynamic_conf {
-  $package = ['httpd','php']
-  package {$package: ensure => 'latest'}
-  file {'/var/www/html':
-    ensure => directory
-    }
-  file {'/var/www/html/index.php':
-    ensure => file,
-    source => 'puppet:///modules/dynamic/index.php'
-    }
- # file {'/etc/httpd/conf/httpd.conf':
- #   ensure => file,
- #   source => 'puppet:///modules/dynamic/httpd.conf'
- #  }
-  file {'/etc/httpd/conf.d/dynamic.conf':
-    ensure => file,
-    source => 'puppet:///modules/dynamic/dynamic.conf',
-    notify => Service['httpd']
-   }
-  service {'httpd':
-    ensure => running,
-    enable => true
-   }
-  file {'/root/readme':
-  ensure => absent
-  }
-}
-
-class static_conf {
-  $package = ['httpd','php']
-  package {$package: ensure => 'installed'} 
-  file {'/var/www/html':
-    ensure => directory
-   }
-  file {'/var/www/html/index.html':
-    ensure => file,
-    source => 'puppet:///modules/static/index.html'
-   }
-#  file {'/etc/httpd/conf/httpd.conf':
-#    ensure => file,
-#    source => 'puppet:///modules/static/httpd.conf'
-#   }
-  file {'/etc/httpd/conf.d/static.conf':
-    ensure => file,
-    source => 'puppet:///modules/static/static.conf',
-    notify => Service['httpd']
-   }
-  service {'httpd':
-    ensure => running, 
-    enable => true
-  }
-  file {'/root/readme':
-  ensure => absent
   }
 }
