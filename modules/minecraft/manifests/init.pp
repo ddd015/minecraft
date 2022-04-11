256096e07c2c9323aac47cd4b0d66590cc0bd807
ddd015 <101343055+ddd015@users.noreply.github.com>
2022-04-11 20:04:44 +0300

    Update init.pp

--- modules/minecraft/manifests/init.pp
+++ modules/minecraft/manifests/init.pp
@@ -5,16 +5,16 @@ class minecraft {
  file {'/opt/minecraft':
    ensure => directory
    }
+ file {'/opt/minecraft/eula.txt':
+   ensure => file,
+   source => 'puppet:///modules/minecraft/eula.txt'
+   }
  include wget
  wget::fetch { "download minecraft":
    source      => 'https://launcher.mojang.com/v1/objects/c8f83c5655308435b3dcf03c06d9fe8740a77469/server.jar',
    destination => '/opt/minecraft/',
    timeout     => 0,
    verbose     => false
-   }
- file {'/opt/minecraft/eula.txt':
-   ensure => file,
-   source => 'puppet:///modules/minecraft/eula.txt'
    }
 #  file_line {'/opt/minecraft/eula.txt':
 #  ensure => present,
