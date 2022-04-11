e53ed98e040a5fb79925e3a269664dcf28422099
ddd015 <101343055+ddd015@users.noreply.github.com>
2022-04-11 14:01:38 +0300

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,11 +1,3 @@
 node 'master' {
-include nginx
-nginx::resource::server { 'static':
-  listen_port => 80,
-  proxy => 'http://192.168.56.6:80',
-  }
-nginx::resource::server { 'dynamic':
-  listen_port => 81,
-  proxy => 'http://192.168.56.7:80',
-  }
+include minecraft
 }
